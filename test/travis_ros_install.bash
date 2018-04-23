e5ba42cf1b8ec2de60c8f5f83d0f9429d0e834a6
Furukawa Yuki <s1400635@sendai-nct.jp>
2018-04-23 02:22:01 +0000

    pipの場所変更

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,11 +1,12 @@
 #!/bin/bash -xve
 
 #requierd packages
+pip install --upgrade pip
 pip install catkin_pkg
 pip install empy
 pip install pyyaml
 pip install rospkg
-pip install --upgrade pip
+#pip install --upgrade pip
 
 #ros install
 cd ..
